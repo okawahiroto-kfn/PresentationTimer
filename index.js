10fad73c8cd97e45542ff76d9218da6bd01a688d
okawahiroto <okawa.hiroto@gmail.com>
2022-01-04 12:54:16 +0900

    appendChild追加

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var btn = document.getElementById('btn');
 
 btn.addEventListener('click', function() {
+  console.log('ボタンが押されました！');
+
   var text = document.getElementById('text');
   text.innerText = 'Hello World!';
-});
 
+  let parentNode = document.getElementById('text');
+  let childNode = document.createElement('div');
+  childNode.appendChild(document.createTextNode('追加！'));
+  parentNode.appendChild(childNode);
+});
