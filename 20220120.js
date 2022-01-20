2d42bafa36ecf6dbe02475b97ecaa9d10ec3e22c
okawahiroto <okawa.hiroto@gmail.com>
2022-01-20 23:54:25 +0900

    表の時間列を合計し、表示

---
+++ 20220120.js
@@ -0,0 +1,19 @@
+let check = document.getElementById('check');
+let table = document.getElementById('table');
+let total = document.getElementById('total');
+
+let sumCells = 0;
+
+check.addEventListener('click', function() {
+  console.log('checkボタンが押されました');
+  console.log(table);
+  console.log(table.rows);
+  console.log(table.rows.length);
+
+  for (let i = 1; i < (table.rows.length - 1); i++) {
+    console.log(table.rows[i].cells[1].innerText);
+    sumCells = sumCells + Number(table.rows[i].cells[1].innerText);
+  };
+  console.log(sumCells);
+  total.innerText = sumCells;
+});
