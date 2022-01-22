83bfce8374610dc2b91af44aef8344229836d7f0
okawahiroto <okawa.hiroto@gmail.com>
2022-01-22 22:33:44 +0900

    表の各行(tr)、削除ボタンにid付与、行数を取得→行を削除させたい。

--- 20220120.js
+++ 20220120.js
@@ -5,26 +5,49 @@ let btnDelete = document.getElementById('btnDelete');
 
 let sumCells = 0;
 
+// 合計ボタンが押された時の処理
 btnSum.addEventListener('click', function() {
-  console.log('checkボタンが押されました');
+  console.log('合計ボタンが押されました');
+
+  // tebleの要素を表示
   console.log(table);
+
+  // tableの各行の要素を表示
   console.log(table.rows);
+
+  // tableの行数を表示
   console.log(table.rows.length);
 
+  // tableの行数分ループ
   for (let i = 1; i < (table.rows.length - 1); i++) {
-    console.log(table.rows[i].cells[1].innerText);
+    // 時間の合計を計算
     sumCells = sumCells + Number(table.rows[i].cells[1].innerText);
+
+    // trにidを付与
+    let row = table.rows[i];
+    row.setAttribute('id', i);
+    console.log(row);
+
+    // 削除ボタンにidを付与
+    let dButton = table.rows[i].cells[2].children[0];
+    dButton.setAttribute('id', i);
+    console.log(dButton);
   };
+
   console.log(sumCells);
   total.innerText = sumCells;
   sumCells = 0;
 });
 
+// 削除ボタンが押された時の処理
 function clickDelete() {
   console.log('deleteボタンが押されました');
-  console.log(btnDelete);
+  console.log(btnDelete.id);
   console.log(btnDelete.parentNode.parentNode.cells[1].innerText);
   console.log(btnDelete.parentNode);
   console.log(btnDelete.parentNode.parentNode);
-
+  console.log(document.getElementById(btnDelete.id));
+  let row = document.getElementById(btnDelete.id);
+  console.log(row);
+  // row.parentNode.removeChild(row);
 };
