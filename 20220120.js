a983ed8e7728b32c103f867725e13bc66403dda4
okawahiroto <okawa.hiroto@gmail.com>
2022-01-23 21:49:06 +0900

    tr・削除ボタンに行番号のid付与(読み込み時)、行の削除、一部OK。コメント参照

--- 20220120.js
+++ 20220120.js
@@ -5,6 +5,24 @@ let btnDelete = document.getElementById('btnDelete');
 
 let sumCells = 0;
 
+window.onload = function() {
+  // alert('loaded');
+  // tableの行数分ループ
+  for (let i = 1; i < (table.rows.length - 1); i++) {
+  sumCells = sumCells + Number(table.rows[i].cells[1].innerText);
+  total.innerText = sumCells;
+
+  // trにidを付与
+  let row = table.rows[i];
+  row.setAttribute('id', i);
+  console.log(row);
+
+  // 削除ボタンにidを付与
+  let dButton = table.rows[i].cells[2].children[0];
+  dButton.setAttribute('id', i);
+  };
+};
+
 // 合計ボタンが押された時の処理
 btnSum.addEventListener('click', function() {
   console.log('合計ボタンが押されました');
@@ -40,14 +58,27 @@ btnSum.addEventListener('click', function() {
 });
 
 // 削除ボタンが押された時の処理
-function clickDelete() {
+function clickDelete(ele) {
   console.log('deleteボタンが押されました');
-  console.log(btnDelete.id);
-  console.log(btnDelete.parentNode.parentNode.cells[1].innerText);
-  console.log(btnDelete.parentNode);
-  console.log(btnDelete.parentNode.parentNode);
-  console.log(document.getElementById(btnDelete.id));
-  let row = document.getElementById(btnDelete.id);
+
+  // 削除ボタンのidを取得
+  let id_value = ele.id;
+  console.log(id_value);
+
+  // tableの各行の要素を取得
+  let row = document.getElementsByTagName('tr')[id_value];
+  console.log(document.getElementsByTagName('tr'));
   console.log(row);
-  // row.parentNode.removeChild(row);
+
+  // ボタンの行の分数を取得
+  console.log(row.cells[1].innerText);
+
+  sumCells = sumCells - Number(row.cells[1].innerText);
+  total.innerText = sumCells;
+
+  row.remove();
+  // ※この後、存在しない(画面に表示されない)tr行があり、行の削除がうまくできない。
+  // 上の行から削除すると、最後、合計行が削除されてしまう。
+  // 下の行から削除するとうまくいく。
 };
+
