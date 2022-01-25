11deb906b58a9a516cfc09ae04201132512ebc77
okawahiroto <okawa.hiroto@gmail.com>
2022-01-25 23:50:16 +0900

    ループでid付与。行の削除OK

--- 20220120.js
+++ 20220120.js
@@ -6,20 +6,20 @@ let btnDelete = document.getElementById('btnDelete');
 let sumCells = 0;
 
 window.onload = function() {
-  // alert('loaded');
   // tableの行数分ループ
   for (let i = 1; i < (table.rows.length - 1); i++) {
-  sumCells = sumCells + Number(table.rows[i].cells[1].innerText);
-  total.innerText = sumCells;
 
   // trにidを付与
   let row = table.rows[i];
   row.setAttribute('id', i);
-  console.log(row);
 
   // 削除ボタンにidを付与
   let dButton = table.rows[i].cells[2].children[0];
   dButton.setAttribute('id', i);
+
+  // 合計計算
+  sumCells = sumCells + Number(table.rows[i].cells[1].innerText);
+  total.innerText = sumCells;
   };
 };
 
@@ -27,34 +27,16 @@ window.onload = function() {
 btnSum.addEventListener('click', function() {
   console.log('合計ボタンが押されました');
 
-  // tebleの要素を表示
-  console.log(table);
-
-  // tableの各行の要素を表示
-  console.log(table.rows);
-
-  // tableの行数を表示
-  console.log(table.rows.length);
+  // ループして合計を出す前に0にする。
+  sumCells = 0;
 
   // tableの行数分ループ
   for (let i = 1; i < (table.rows.length - 1); i++) {
-    // 時間の合計を計算
-    sumCells = sumCells + Number(table.rows[i].cells[1].innerText);
 
-    // trにidを付与
-    let row = table.rows[i];
-    row.setAttribute('id', i);
-    console.log(row);
-
-    // 削除ボタンにidを付与
-    let dButton = table.rows[i].cells[2].children[0];
-    dButton.setAttribute('id', i);
-    console.log(dButton);
-  };
-
-  console.log(sumCells);
+  // 合計計算
+  sumCells = sumCells + Number(table.rows[i].cells[1].innerText);
   total.innerText = sumCells;
-  sumCells = 0;
+  };
 });
 
 // 削除ボタンが押された時の処理
@@ -63,22 +45,36 @@ function clickDelete(ele) {
 
   // 削除ボタンのidを取得
   let id_value = ele.id;
-  console.log(id_value);
 
   // tableの各行の要素を取得
   let row = document.getElementsByTagName('tr')[id_value];
-  console.log(document.getElementsByTagName('tr'));
-  console.log(row);
 
-  // ボタンの行の分数を取得
-  console.log(row.cells[1].innerText);
-
+  // 合計を減算
   sumCells = sumCells - Number(row.cells[1].innerText);
   total.innerText = sumCells;
 
+  // tableから削除
   row.remove();
-  // ※この後、存在しない(画面に表示されない)tr行があり、行の削除がうまくできない。
-  // 上の行から削除すると、最後、合計行が削除されてしまう。
-  // 下の行から削除するとうまくいく。
+
+  // ループして合計を出す前に0にする。
+  sumCells = 0;
+
+  // tableの行数分ループしてidを更新
+  for (let i = 1; i < (table.rows.length - 1); i++) {
+
+  // trにidを付与
+  let row = table.rows[i];
+  row.setAttribute('id', i);
+
+  row.cells[0].innerText = i + '.';
+
+  // 削除ボタンにidを付与
+  let dButton = table.rows[i].cells[2].children[0];
+  dButton.setAttribute('id', i);
+
+  // 合計計算
+  sumCells = sumCells + Number(table.rows[i].cells[1].innerText);
+  total.innerText = sumCells;
+  };
 };
 
