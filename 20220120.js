6553a35e44b26d3223056dcb7ab3c142843d61e4
okawahiroto <okawa.hiroto@gmail.com>
2022-01-26 04:32:48 +0900

    入力欄の内容→入力ボタン→表へ反映

--- 20220120.js
+++ 20220120.js
@@ -1,7 +1,9 @@
-let check = document.getElementById('btnSum');
+let btnInput = document.getElementById('btnInput');
 let table = document.getElementById('table');
 let total = document.getElementById('total');
 let btnDelete = document.getElementById('btnDelete');
+let itemText = document.getElementById('itemText');
+let itemTime = document.getElementById('itemTime');
 
 let sumCells = 0;
 
@@ -23,9 +25,29 @@ window.onload = function() {
   };
 };
 
-// 合計ボタンが押された時の処理
-btnSum.addEventListener('click', function() {
-  console.log('合計ボタンが押されました');
+// 入力ボタンが押された時の処理
+btnInput.addEventListener('click', function() {
+  console.log('入力ボタンが押されました');
+
+  // 合計行の上に新しい行を追加
+  let newRow = table.insertRow(table.rows.length - 1);
+
+  // 各行にidを付与
+  newRow.setAttribute('id', table.rows.length - 2);
+
+  // セルを追加
+  let newCell01 = newRow.insertCell();
+  let newCell02 = newRow.insertCell();
+  let newCell03 = newRow.insertCell();
+
+  // 項目欄のナンバリング
+  newCell01.innerText = itemText.value;
+
+  // 時間欄に入力された時間を追加
+  newCell02.innerText = itemTime.value;
+
+  // 削除ボタンを追加
+  newCell03.innerHTML = '<button id="' + (table.rows.length - 2) + '" onclick="clickDelete(this)">削除</button>';
 
   // ループして合計を出す前に0にする。
   sumCells = 0;
@@ -67,7 +89,7 @@ function clickDelete(ele) {
   row.setAttribute('id', i);
 
   // 項目欄のナンバリング
-  row.cells[0].innerText = i + '.';
+  // row.cells[0].innerText = i + '.';
 
   // 削除ボタンにidを付与
   let dButton = table.rows[i].cells[2].children[0];
